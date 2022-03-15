cc589514783210206836212fe6fd0cd1c213327f
houngkyung <houngkyung.kim@gmail.com>
2022-03-15 16:57:53 +0900

    first version of fashion youtube trend page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.css";
+import React, { useEffect, useState } from "react";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
+  const [videos, setVideos] = useState([]);
+  // Get Week of Year
+  const now = new Date();
+  const onejan = new Date(now.getFullYear(), 0, 1);
+  const weekNumber =
+    Math.ceil(
+      ((now.getTime() - onejan.getTime()) / 86400000 + onejan.getDay() + 1) / 7
+    ) - 1;
+  //
+
+  useEffect(() => {
+    let result = [];
+    let filteredResult = [];
+    let video;
+
+    try {
+      result = require("./collected.json");
+    } catch (error) {
+      console.log(error);
+    }
+    console.log(result);
+    // console.log(result[0])
+    // console.log(result[0].viewCount)
+    // console.log(result[0].videoId)
+
+    for (const video of result) {
+      console.log(video.weekNumber);
+      if (
+        video.weekNumber >= weekNumber &&
+        weekNumber + 1 >= video.weekNumber &&
+        video.viewCount > 100000
+      ) {
+        filteredResult.push(video);
+      }
+    }
+
+    // setVideos(result)
+    setVideos(filteredResult);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="App">Fashion Youtube 트렌드 분석</h1>
+      <span>(v1.0, 2022-03-15 )</span>
+      <p>
+        오늘은 {now.toDateString()} ({weekNumber}주차) 입니다.
+      </p>
+      <p>패션 유튜버들은 이맘 때즘 이런 영상들을 만들었습니다</p>
+      <VideoList videos={videos} />
+    </>
   );
 }
 
---
+++ src/components/video_item/video_item.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import VideoList from "../video_list/video_list";
+import styles from "./video_item.module.css";
+// import React, { useEffect, useState } from "react";
+
+const VideoItem = ({ video }) => {
+  // const VideoItem = ({ video: { snippet } }) => {
+  // const base = "https://www.youtube.com/watch?v=";
+  return (
+    <li className={styles.container}>
+      <div className={styles.video}>
+        <a href={video.link}>
+          <img
+            className={styles.thumbnail}
+            src={video.thumbnail}
+            alt={video.title}
+          ></img>
+        </a>
+        <div className={styles.metadata}>
+          <a href={VideoList.link}>
+            <p className={styles.title}>{video.title}</p>
+            <p className={styles.channel}>{video.channel}</p>
+            <p className={styles.viewCount}>
+              조회수 {video.viewCount} · <span>{video["year-month-date"]}</span>
+            </p>
+          </a>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default VideoItem;
